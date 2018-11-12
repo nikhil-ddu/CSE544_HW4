96b5e96ac77716ac8d2eb5ded867ab7885b684ec
aksinghal5590 <aksinghal@cs.stonybrook.edu>
2018-11-12 14:25:00 -0500

    Changes in q6 calculation

--- q6/q6.py
+++ q6/q6.py
@@ -3,7 +3,10 @@ import numpy
 sample = [1.87, 1.29, 2.01, 0.93, 1.02, 2.78, 2.33, 1.65, 0.50, 0.99]
 
 x = numpy.mean(sample)
-s = numpy.var(sample)
+s = numpy.sqrt(numpy.var(sample))
 
 print(x)
 print(s)
+
+t = ((x - 1.5) * numpy.sqrt(10))/s
+print(t)
